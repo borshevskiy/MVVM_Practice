d53daaef75016d38c19b5831a22baf75ea5ad13d
SergeBorshevskiy <sk8er1001@mail.ru>
2021-09-28 19:20:19 +0600

    ViewBinding MainActivity.kt and initialize NavController

--- app/src/main/java/com/borshevskiy/mvvp_practice/MainActivity.kt
+++ app/src/main/java/com/borshevskiy/mvvp_practice/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.borshevskiy.mvvp_practice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.borshevskiy.mvvp_practice.databinding.ActivityMainBinding
+import com.borshevskiy.mvvp_practice.utils.ACTIVITY_APP
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mNavController: NavController
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ACTIVITY_APP = this
+        mNavController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        setSupportActionBar(binding.toolbar)
+        title = getString(R.string.title)
     }
 }
---
+++ app/src/main/java/com/borshevskiy/mvvp_practice/utils/constants.kt
@@ -0,0 +1,5 @@
+package com.borshevskiy.mvvp_practice.utils
+
+import com.borshevskiy.mvvp_practice.MainActivity
+
+lateinit var ACTIVITY_APP:MainActivity
